cd82a2cdf33d0c56fae3a528e1755f2e85d62ab4
nnamdi <uzonnamdi31@gmail.com>
2025-08-06 18:55:11 +0100

    new

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -2,7 +2,7 @@ import Button from "../components/Button";
 
 export default function Hero() {
     return (
-        <div className="mt-24 text-white px-6 lg:px-40 flex flex-col-reverse lg:flex-row justify-center lg:justify-between items-center">
+        <div className="mt-10 lg:mt-24 text-white px-6 lg:px-40 flex flex-col-reverse lg:flex-row justify-center lg:justify-between items-center">
             <div className="flex flex-col items-center lg:items-start">
                 <h1 className="text-center lg:text-left text-4xl font-semibold w-full lg:w-[70%]">Get Quick Health Insights, Find Care Near You!</h1>
                 <p className="w-full lg:w-[60%] text-xl my-6 text-center lg:text-left">
@@ -10,7 +10,7 @@ export default function Hero() {
                 </p>
                 <Button>Check Your Symptoms Now</Button>
             </div>
-            <img src="/new4.png" alt="" className="w-[50%]" />
+            <img src="/new4.png" alt="" className="w-[70%] lg:w-[50%]" />
             
         </div>
     )
