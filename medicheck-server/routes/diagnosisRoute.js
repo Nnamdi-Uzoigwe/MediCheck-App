4be76853537b45edb4096704fe2d1a82592a89b4
nnamdi <uzonnamdi31@gmail.com>
2025-08-14 01:01:26 +0100

    new

--- medicheck-server/routes/diagnosisRoute.js
+++ medicheck-server/routes/diagnosisRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const diagnosisController = require("../controllers/diagnosisController");
-const authMiddleware = require("../middlewares/authMiddleware");
+const authMiddleware = require("../middleware/authMiddleware");
 
 router.get("/:id", authMiddleware, diagnosisController.getDiagnosis);
 router.get("/", authMiddleware, diagnosisController.getDiagnoses);
--- medicheck-server/routes/symptomRoute.js
+++ medicheck-server/routes/symptomRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const symptomController = require("../controllers/symptomController");
-const authMiddleware = require("../middlewares/authMiddleware");
+const authMiddleware = require("../middleware/authMiddleware");
 
 router.post("/", authMiddleware, symptomController.submitSymptoms);
 router.get("/history", authMiddleware, symptomController.getSymptomHistory);
