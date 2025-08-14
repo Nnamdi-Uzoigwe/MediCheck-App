43552bfddd3dfe1d27707de850ff36ae0ed79a96
nnamdi <uzonnamdi31@gmail.com>
2025-08-14 01:09:16 +0100

    new

--- medicheck-server/routes/symptomRoute.js
+++ medicheck-server/routes/symptomRoute.js
@@ -4,6 +4,6 @@ const symptomController = require("../controllers/symptomController");
 const authMiddleware = require("../middleware/authMiddleware");
 
 router.post("/", authMiddleware, symptomController.submitSymptoms);
-router.get("/history", authMiddleware, symptomController.getSymptomHistory);
+// router.get("/history", authMiddleware, symptomController.getSymptomHistory);
 
 module.exports = router;
